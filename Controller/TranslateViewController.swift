edd7a28f1afb25a89671a3377918a22db4a6398c
Maxime6 <maxime.tanter@hotmail.fr>
2019-01-30 19:03:04 +0100

    Implementation of the responsive design of the translate interface and create TranslateService and TranslateData files in the model

--- Controller/TranslateViewController.swift
+++ Controller/TranslateViewController.swift
@@ -9,22 +9,17 @@
 import UIKit
 
 class TranslateViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var baseLanguageTextField: UITextField!
+    @IBOutlet weak var targetLanguageLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
 
-        // Do any additional setup after loading the view.
+    @IBAction func translateButton(_ sender: Any) {
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
---
+++ Model/TranslateData.swift
@@ -0,0 +1,13 @@
+//
+//  TranslateData.swift
+//  AnAmericanDream
+//
+//  Created by Yves TANTER on 30/01/2019.
+//  Copyright © 2019 Maxime Tanter. All rights reserved.
+//
+
+import Foundation
+
+struct TranslateData: Decodable {
+    
+}
